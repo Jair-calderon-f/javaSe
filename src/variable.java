a10f5bf8a8d341c9d55619f8b2b1ce43288bfa93
jair calderon <jaircalderon1608@gmail.com>
2021-03-10 14:05:22 -0600

    varibales

---
+++ src/variable.java
@@ -0,0 +1,9 @@
+public class variable {
+    public static void main(String[] args) {
+        //declarando una variable
+        int salary=1000; //entero
+        String employeeName="jair";
+        System.out.printf("variable "+salary);
+
+    }
+}
