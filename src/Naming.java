ac45561bd5d4815427405390310bce822e60e033
jair calderon <jaircalderon1608@gmail.com>
2021-03-10 15:19:11 -0600

    naming

---
+++ src/Naming.java
@@ -0,0 +1,17 @@
+public class Naming {
+    public static void main(String[] args) {
+        int celphone=12345;
+        int CelPhone=547896;
+        System.out.println(celphone);
+        System.out.println(celphone);
+
+        String $countryName="spain";
+        String _backgroundColor="Green";
+        String currency$="MX";
+        String backgorund_Color="blue";
+        //constantes
+        int POSITION=-5;
+        int MAX_WIDTH=999;
+        int MIN_WIDTH=1;
+    }
+}
