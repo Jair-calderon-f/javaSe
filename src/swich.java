30f065dd1c2bd18d50850b6fba6097791aa773b9
jair calderon <jaircalderon1608@gmail.com>
2021-03-10 20:06:57 -0600

    swich

---
+++ src/swich.java
@@ -0,0 +1,21 @@
+public class swich {
+    public static void main(String[] args) {
+    String colorModeSelected="Dar";
+        switch (colorModeSelected){
+            case "light":
+                System.out.println("selecccionaste light Mode");
+                break;
+            case "night":
+                System.out.println("seleccionaste night Mode");
+                break;
+            case "blue darck":
+                System.out.println("seleccionaste blue darck mode");
+                break;
+            case "Dark":
+                System.out.println("seleccionaste darck");
+                break;
+            default:
+                System.out.println("selecciona una opcion sea correcta");
+        }
+    }
+}
