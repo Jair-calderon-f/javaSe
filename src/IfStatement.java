d406150fd39bcf3dd23ca02e9289be8fdc9f0354
jair calderon <jaircalderon1608@gmail.com>
2021-03-10 18:55:46 -0600

    if

---
+++ src/IfStatement.java
@@ -0,0 +1,12 @@
+public class IfStatement {
+    public static void main(String[] args) {
+        boolean idBluetoothEnable=false;
+        int fileSend=3;
+        if(idBluetoothEnable){
+            fileSend++;
+            System.out.println("archivo enviado");
+        }else{
+            System.out.println("archivo no enviado");
+        }
+    }
+}
